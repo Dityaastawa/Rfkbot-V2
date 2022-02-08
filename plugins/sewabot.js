d7753da74d853f2aaed62b1bc9d31f3546797520
dityaastawa <90491847+Dityaastawa@users.noreply.github.com>
2022-02-08 12:46:18 +0800

    Add files via upload

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -11,17 +11,17 @@ let handler = async(m, { conn }) => {
 Silahkan klik pada "List Harga" untuk melihat list.
 
 Pembayaran:
-Pulsa (ISAT): 085730903853 (+5.000)
-Gopay: 085730903853
+Pulsa (IM3): 085792429140 (+5.000)
+Gopay: -
 `.trim()
     const button = {
         buttonText: 'List Harga',
         description: kamisato,
         sections:  [{title: "Silahkan di pilih", rows: [
-        {title: '2 Bulan', description: "Rp30.000\nSewa bot tanpa batasan waktu.", rowId:".masuk"},
-        {title: '1 Bulan', description: "Rp20.000\nSewa bot selama 1 bulan.", rowId:".masuk"},
-        {title: '1 Minggu', description: "Rp15.000\nSewa bot selama 1 minggu.", rowId:".masuk"},
-        {title: '3 hari', description: "Rp5.000\nSewa bot selama 3 hari.", rowId:".masuk"},
+        {title: '4 Minggu', description: "Rp15.000\nSewa bot tanpa batasan waktu.", rowId:".masuk"},
+        {title: '3 Minggu', description: "Rp10.000\nSewa bot selama 1 bulan.", rowId:".masuk"},
+        {title: '1 Minggu', description: "Rp5.000\nSewa bot selama 1 minggu.", rowId:".masuk"},
+        {title: '2 Minggu', description: "Rp8.000\nSewa bot selama 3 hari.", rowId:".masuk"},
         {title: 'Owner', description: "Chat owner nya jika ada perlu.", rowId:".owner"},
         {title: 'Rules', description: "Kebijakan Privasi, Syarat Ketentuan dan Peraturan.", rowId:".rules"},
        ] }],
