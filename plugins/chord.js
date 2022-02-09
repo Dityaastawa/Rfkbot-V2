181b4d372116d54e65a3dba76ccf73b77721283a
dityaastawa <90491847+Dityaastawa@users.noreply.github.com>
2022-02-09 15:07:43 +0800

    Add files via upload

--- plugins/chord.js
+++ plugins/chord.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text, usedPrefix }) => {
 
     if (!text) return conn.reply(m.chat, 'Contoh penggunaan: ' + usedPrefix + 'chord hanya rindu', m)
     await m.reply(global.wait)
-    axios.get(`https://docs-jojo.herokuapp.com/api/chord?q=` + text)
+    axios.get(`https://docs-jojo.herokuapp.com/api/chord?q=aku+bukan+boneka)
         .then((res) => {
           let hasil = `*• Chord Lagu ${text} :*\n${res.data.result}`
             conn.reply(m.chat, hasil, m)
