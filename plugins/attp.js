8e118182f3ee3f054ce6ae91c11bc24802e45d9b
dityaastawa <90491847+Dityaastawa@users.noreply.github.com>
2022-02-10 13:15:25 +0800

    Add files via upload

--- plugins/attp.js
+++ plugins/attp.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn, text }) => {
 let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-conn.sendFile(m.chat, global.API('https://api.lolhuman.xyz/api/attp?apikey=bca5b104e231d4708ce6dfa8&', '/attp', { file: '', text: teks }), 'attp.webp', '', m, { asSticker: true })
+conn.sendFile(m.chat, global.API('https://api.lolhuman.xyz/api/attp?apikey=bca5b104e231d4708ce6dfa8&text=', '/attp', { file: '', text: teks }), 'attp.webp', '', m, { asSticker: true })
 }
 handler.help = ['attp <teks>']
 handler.tags = ['sticker']
